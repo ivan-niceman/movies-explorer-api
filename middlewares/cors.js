f54c5c3e4cd8fcee2bcc15f7eb05c635fbaad03d
ivan-niceman <sn.iv@bk.ru>
2023-08-13 12:54:04 +0700

    исправил api

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,6 @@
 const allowedCors = [
-  'http://nice-man.diploma.nomoreparties.sbs',
-  'https://nice-man.diploma.nomoreparties.sbs',
+  'http://api.nice-man.diploma.nomoreparties.sbs',
+  'https://api.nice-man.diploma.nomoreparties.sbs',
   'http://localhost:3000',
   'https://localhost:3000',
 ];
